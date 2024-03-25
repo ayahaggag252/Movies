336bd94ecbdac0bd7e4258896419bac52e342168
ayahaggag252 <164349413+ayahaggag252@users.noreply.github.com>
2024-03-25 17:51:56 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:movieapp/pages/home_page.dart';
+import 'package:movieapp/pages/main_page.dart';
+import 'package:movieapp/pages/details_page.dart';
+import 'package:movieapp/pages/splash_page.dart';
+import 'package:movieapp/themes/themes.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Movieapp',
+      themeMode: ThemeMode.system,
+      theme: MyThemes.lightTheme(context),
+      darkTheme: MyThemes.darkTheme(context),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/splash',
+      getPages: [
+        GetPage(name: '/splash', page: () => const SplashPage()),
+        GetPage(name: '/main', page: () => const MainPage()),
+        GetPage(name: '/home', page: () => const HomePage()),
+        GetPage(name: '/deatils', page: () => const DetailsPage()),
+      ],
+    );
+  }
+}
